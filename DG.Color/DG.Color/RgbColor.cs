53b438e8e9ffb28d5ad72baf8bc8b2f8ed25badc
dennisvg111 <dennisvg111@gmail.com>
2023-03-18 20:54:29 +0100

    Add conversion logic

---
+++ DG.Color/DG.Color/RgbColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DG.Color
+{
+    public class RgbColor : BaseColor<RgbColor>
+    {
+        protected override RgbaValues ConvertToRgba()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override RgbColor CreateFromRgba(RgbaValues rgbaValues)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DG.Color/DG.Color/RgbaValues.cs
+++ DG.Color/DG.Color/RgbaValues.cs
@@ -8,24 +8,43 @@ namespace DG.Color
 {
     public readonly struct RgbaValues : IEquatable<RgbaValues>
     {
-        private readonly float _alpha;
         private readonly byte _red;
         private readonly byte _green;
         private readonly byte _blue;
+        private readonly float _alpha;
 
-        public RgbaValues(float alpha, byte red, byte green, byte blue)
+        public RgbaValues(byte red, byte green, byte blue, float alpha = 1)
         {
-            _alpha = alpha;
             _red = red;
             _green = green;
             _blue = blue;
+            _alpha = alpha;
         }
-
-        public float Alpha => _alpha;
 
         public byte Red => _red;
         public byte Green => _green;
         public byte Blue => _blue;
+
+        /// <summary>
+        /// The alpha value, on a scale from 0 (transparent) to 1 (opaque).
+        /// </summary>
+        public float Alpha => _alpha;
+
+        internal byte AlphaByte
+        {
+            get
+            {
+                if (Alpha >= 1.0)
+                {
+                    return 255;
+                }
+                if (Alpha <= 0.0)
+                {
+                    return 0;
+                }
+                return (byte)Math.Round(Alpha * 255.0);
+            }
+        }
 
         public override bool Equals(object obj)
         {
@@ -34,20 +53,22 @@ namespace DG.Color
 
         public bool Equals(RgbaValues other)
         {
-            return _alpha == other._alpha &&
-                   _red == other._red &&
-                   _green == other._green &&
-                   _blue == other._blue;
+            return _red == other._red && _green == other._green && _blue == other._blue && _alpha == other._alpha;
         }
 
         public override int GetHashCode()
         {
-            int hashCode = 1838352370;
-            hashCode = hashCode * -1521134295 + _alpha.GetHashCode();
-            hashCode = hashCode * -1521134295 + _red.GetHashCode();
-            hashCode = hashCode * -1521134295 + _green.GetHashCode();
-            hashCode = hashCode * -1521134295 + _blue.GetHashCode();
-            return hashCode;
+            unchecked // Overflow is fine, just wrap
+            {
+                int hash = 17;
+                int multiplyPrime = 486187739;
+                // Suitable nullity checks etc, of course :)
+                hash = hash * multiplyPrime + _red.GetHashCode();
+                hash = hash * multiplyPrime + _green.GetHashCode();
+                hash = hash * multiplyPrime + _blue.GetHashCode();
+                hash = hash * multiplyPrime + _alpha.GetHashCode();
+                return hash;
+            }
         }
 
         public static bool operator ==(RgbaValues left, RgbaValues right)
