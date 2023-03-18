c779b811f7604bca210f460a87b74b0f6389bd93
dennisvg111 <dennisvg111@gmail.com>
2023-03-18 13:33:39 +0100

    Add rgba values struct

---
+++ DG.Color/DG.Color/RgbaValues.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DG.Color
+{
+    public readonly struct RgbaValues : IEquatable<RgbaValues>
+    {
+        private readonly float _alpha;
+        private readonly byte _red;
+        private readonly byte _green;
+        private readonly byte _blue;
+
+        public RgbaValues(float alpha, byte red, byte green, byte blue)
+        {
+            _alpha = alpha;
+            _red = red;
+            _green = green;
+            _blue = blue;
+        }
+
+        public float Alpha => _alpha;
+
+        public byte Red => _red;
+        public byte Green => _green;
+        public byte Blue => _blue;
+
+        public override bool Equals(object obj)
+        {
+            return obj is RgbaValues values && Equals(values);
+        }
+
+        public bool Equals(RgbaValues other)
+        {
+            return _alpha == other._alpha &&
+                   _red == other._red &&
+                   _green == other._green &&
+                   _blue == other._blue;
+        }
+    }
+}
