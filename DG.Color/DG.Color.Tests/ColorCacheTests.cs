3ffda4f3e564a7057dac043abc97b20f8b2119f0
dennisvg111 <dennisvg111@gmail.com>
2023-05-12 14:56:48 +0200

    Add color cache class with tests

---
+++ DG.Color/DG.Color.Tests/ColorCacheTests.cs
@@ -0,0 +1,39 @@
+﻿using DG.Color.Utilities;
+using Xunit;
+
+namespace DG.Color.Tests
+{
+    public class ColorCacheTests
+    {
+        [Fact]
+        public void CacheTest()
+        {
+            ColorTransformationWithCounter counter = new ColorTransformationWithCounter();
+            ColorCache cache = new ColorCache((c) => counter.Transform(c));
+            RgbValues r1 = new RgbValues(128, 0, 0);
+            RgbValues r2 = new RgbValues(255, 255, 255);
+
+            var transformed = cache.GetResultOrCreate(r1);
+            transformed = cache.GetResultOrCreate(r1);
+
+            Assert.Equal(1, counter.Calls);
+
+            transformed = cache.GetResultOrCreate(r2);
+            transformed = cache.GetResultOrCreate(r1);
+
+            Assert.Equal(2, counter.Calls);
+        }
+
+        private class ColorTransformationWithCounter
+        {
+            private int _calls;
+            public int Calls => _calls;
+
+            public RgbValues Transform(RgbValues values)
+            {
+                _calls++;
+                return new RgbValues(values.Red, values.Red, values.Red);
+            }
+        }
+    }
+}
---
+++ DG.Color/DG.Color/Utilities/ColorCache.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DG.Color.Utilities
+{
+    /// <summary>
+    /// Provides a way to cache the results of a color transformation.
+    /// </summary>
+    public class ColorCache
+    {
+        private readonly Func<RgbValues, RgbValues> _colorFunction;
+        private readonly Dictionary<RgbValues, Lazy<RgbValues>> _transformedColors;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ColorCache"/> with the given <paramref name="colorFunction"/>.
+        /// </summary>
+        /// <param name="colorFunction"></param>
+        public ColorCache(Func<RgbValues, RgbValues> colorFunction)
+        {
+            _colorFunction = colorFunction;
+            _transformedColors = new Dictionary<RgbValues, Lazy<RgbValues>>(256 * 256 * 256);
+        }
+
+        /// <summary>
+        /// Applies the color transformation to the given color, or returns a cached result if it exists.
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        public RgbValues GetResultOrCreate(RgbValues color)
+        {
+            if (!_transformedColors.ContainsKey(color))
+            {
+                _transformedColors[color] = new Lazy<RgbValues>(() => _colorFunction(color));
+            }
+            return _transformedColors[color].Value;
+        }
+    }
+}
